f4c2c262fb3b44b0b5b13767e4c6c2b430a096c6
galinheta14 <sarahrocha201911@gmail.com>
2024-11-29 14:46:36 -0300

    Add files via upload

---
+++ respostas.js
@@ -0,0 +1,288 @@
+criaCartao(
+    'Filme: O Exorcista',
+    'Qual é o nome da garota possuída em O Exorcista?',
+    'Regan MacNeil'
+)
+
+criaCartao(
+    'Filme: O Iluminado',
+    'Quem interpretou Jack Torrance em O Iluminado?',
+    'Jack Nicholson'
+)
+
+criaCartao(
+    'Filme: O Sexto Sentido',
+    'Qual é o grande segredo de Bruce Willis em O Sexto Sentido?',
+    'Ele é um fantasma.'
+)
+
+criaCartao(
+    'Filme: Psicose',
+    'Qual é o nome do vilão em Psicose?',
+    'Norman Bates'
+)
+
+criaCartao(
+    'Filme: It: A Coisa',
+    'Qual é o nome do palhaço em It: A Coisa?',
+    'Pennywise'
+)
+
+criaCartao(
+    'Filme: A Noite dos Mortos-Vivos',
+    'Quem dirigiu o filme A Noite dos Mortos-Vivos?',
+    'George A. Romero'
+)
+
+criaCartao(
+    'Filme: O Massacre da Serra Elétrica',
+    'Qual é o nome do assassino em O Massacre da Serra Elétrica?',
+    'Leatherface'
+)
+
+criaCartao(
+    'Filme: Hereditary',
+    'Quem é a personagem principal de Hereditary?',
+    'Annie Graham'
+)
+
+criaCartao(
+    'Filme: O Labirinto do Fauno',
+    'Qual é o nome da jovem protagonista de O Labirinto do Fauno?',
+    'Ofelia'
+)
+
+criaCartao(
+    'Filme: Annabelle',
+    'De onde vem a boneca Annabelle?',
+    'De uma loja de antiguidades'
+)
+
+criaCartao(
+    'Filme: Midsommar',
+    'Em que tipo de ambiente ocorre o filme Midsommar?',
+    'Uma pequena vila na Suécia'
+)
+
+criaCartao(
+    'Filme: Atividade Paranormal',
+    'O que é capturado pelas câmeras em Atividade Paranormal?',
+    'Fenômenos paranormais em uma casa'
+)
+
+criaCartao(
+    'Filme: O Chamado',
+    'Qual é o nome da fita amaldiçoada em O Chamado?',
+    'A fita de vídeo'
+)
+
+criaCartao(
+    'Filme: A Bruxa',
+    'Qual é o tema central do filme A Bruxa?',
+    'Uma família sendo assombrada por uma bruxa na Nova Inglaterra'
+)
+
+criaCartao(
+    'Filme: Us',
+    'Em Us, o que são os "doppelgängers"?',
+    'Versões malignas das pessoas'
+)
+
+criaCartao(
+    'Filme: A Noite Do Terror',
+    'Qual é o nome do diretor de A Noite do Terror?',
+    'John Carpenter'
+)
+
+criaCartao(
+    'Filme: O Babadook',
+    'O que é o Babadook?',
+    'Uma entidade sobrenatural que se manifesta por meio de um livro infantil.'
+)
+
+criaCartao(
+    'Filme: A Chave do Mal',
+    'Onde se passa a história de A Chave do Mal?',
+    'Em uma plantação no sul dos Estados Unidos'
+)
+
+criaCartao(
+    'Filme: A Visita',
+    'Quem é o diretor de A Visita?',
+    'M. Night Shyamalan'
+)
+
+criaCartao(
+    'Filme: O Hospedeiro',
+    'Qual é o monstro que aparece em O Hospedeiro?',
+    'Um enorme ser mutante surgido de um experimento científico'
+)
+
+criaCartao(
+    'Filme: A Entidade',
+    'O que o protagonista de A Entidade encontra em sua nova casa?',
+    'Filmes caseiros com registros de assassinatos'
+)
+
+criaCartao(
+    'Filme: Sobrenatural',
+    'O que acontece com a família no filme Sobrenatural?',
+    'Eles se mudam para uma casa onde ocorrem eventos paranormais'
+)
+
+criaCartao(
+    'Filme: O Corpo',
+    'O que é essencial para a história de O Corpo?',
+    'O desaparecimento de uma mulher e a busca por seu corpo'
+)
+
+criaCartao(
+    'Filme: O Nevoeiro',
+    'Qual é o elemento chave de O Nevoeiro?',
+    'Um nevoeiro que esconde criaturas mortais'
+)
+
+criaCartao(
+    'Filme: Poltergeist',
+    'Qual é a razão para os poltergeists se manifestarem na casa de Poltergeist?',
+    'A casa foi construída em um cemitério indígena'
+)
+
+criaCartao(
+    'Filme: Cemitério Maldito',
+    'O que acontece com as pessoas que enterram entes queridos no Cemitério Maldito?',
+    'Elas retornam do além, mas com comportamentos estranhos'
+)
+
+criaCartao(
+    'Filme: A Morte te Dá Parabéns',
+    'Em A Morte te Dá Parabéns, o que a protagonista faz?',
+    'Ela revive o dia de sua morte repetidamente até descobrir quem a matou'
+)
+
+criaCartao(
+    'Filme: Scream - Gritando Agora',
+    'Qual é o nome do assassino em Scream?',
+    'Ghostface'
+)
+
+criaCartao(
+    'Filme: A Maldição de Hill House',
+    'O que é Hill House em A Maldição de Hill House?',
+    'Uma mansão assombrada que é o centro da história'
+)
+
+criaCartao(
+    'Filme: A Freira',
+    'Qual é o nome da entidade demoníaca em A Freira?',
+    'Valak'
+)
+
+criaCartao(
+    'Filme: O Ritual',
+    'Em O Ritual, onde o grupo de amigos se perde?',
+    'Em uma floresta na Suécia'
+)
+
+criaCartao(
+    'Filme: O Grito',
+    'Quem é a personagem principal em O Grito?',
+    'Karen Davis'
+)
+
+criaCartao(
+    'Filme: O Homem nas Sombras',
+    'Qual é o segredo do vilão em O Homem nas Sombras?',
+    'Ele é invisível'
+)
+
+criaCartao(
+    'Filme: O Que Fazemos nas Sombras',
+    'Qual é o gênero de O Que Fazemos nas Sombras?',
+    'Comédia de terror sobre vampiros'
+)
+
+criaCartao(
+    'Filme: Noite dos Mortos-Vivos (1968)',
+    'Qual é o papel da televisão em Noite dos Mortos-Vivos?',
+    'Ela transmite notícias sobre a invasão zumbi'
+)
+
+criaCartao(
+    'Filme: O Lamento',
+    'Quem é o protagonista de O Lamento?',
+    'Um detetive investigando um crime com origens sobrenaturais'
+)
+
+criaCartao(
+    'Filme: O Enigma do Outro Mundo',
+    'Em O Enigma do Outro Mundo, o que os personagens descobrem sobre a criatura alienígena?',
+    'Ela pode imitar qualquer ser vivo.'
+)
+
+criaCartao(
+    'Filme: The Witch',
+    'O que a jovem protagonista de The Witch enfrenta?',
+    'Uma bruxa em uma floresta isolada'
+)
+
+criaCartao(
+    'Filme: A Chamada',
+    'O que acontece com quem assiste à fita de A Chamada?',
+    'Eles morrem após sete dias.'
+)
+
+criaCartao(
+    'Filme: Luzes de Posição',
+    'Em Luzes de Posição, qual é o mistério envolvendo o novo trabalho de um segurança?',
+    'Ele descobre eventos estranhos em um prédio abandonado'
+)
+
+criaCartao(
+    'Filme: O Ritual',
+    'Em O Ritual, o que é encontrado na floresta?',
+    'Um templo antigo e uma criatura monstruosa'
+)
+
+criaCartao(
+    'Filme: Vingança Sangrenta',
+    'Qual é a trama principal de Vingança Sanguinária?',
+    'Uma mulher vingando-se de um ataque brutal'
+)
+
+criaCartao(
+    'Filme: O Príncipe das Sombras',
+    'O que é o "Príncipe das Sombras" em O Príncipe das Sombras?',
+    'Uma entidade demoníaca que deseja escapar para o mundo real'
+)
+
+criaCartao(
+    'Filme: A Colina Escarlate',
+    'Onde se passa a história de A Colina Escarlate?',
+    'Em uma mansão isolada na Inglaterra'
+)
+
+criaCartao(
+    'Filme: O Outro Lado da Porta',
+    'O que é o portal em O Outro Lado da Porta?',
+    'Uma porta que permite trazer de volta os mortos'
+)
+
+criaCartao(
+    'Filme: O Vingador',
+    'Quem é o protagonista de O Vingador?',
+    'Uma mulher que busca vingança após ser vítima de violência'
+)
+
+criaCartao(
+    'Filme: O Último Suspiro',
+    'Em O Último Suspiro, qual é o maior mistério?',
+    'A morte de várias pessoas misteriosas em um hospital'
+)
+
+criaCartao(
+    'Filme: Sob a Sombra',
+    'Em Sob a Sombra, qual é o contexto da história?',
+    'A história se passa durante a Guerra Irã-Iraque, onde uma mãe e sua filha enfrentam eventos paranormais em um prédio destruído.'
+)
+
